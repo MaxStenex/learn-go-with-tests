e613c5a6780d0759990b73e739a4ff553adab75e
Maxim <yourstenex@gmail.com>
2023-11-12 18:06:04 +0300

    helper get prefix function, refactor

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,14 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to the people", func(t *testing.T) {
-		got := Hello("Maxim")
-		want := "Hello, Maxim"
-		assertCorrectMessage(t, got, want)
+		spanishResultGot := Hello("Maxim", "Spanish")
+		spanishResultWant := "Hola, Maxim"
+
+		frenchResultGot := Hello("Maxim", "French")
+		frenchResultWant := "Bonjour, Maxim"
+
+		assertCorrectMessage(t, spanishResultGot, spanishResultWant)
+		assertCorrectMessage(t, frenchResultGot, frenchResultWant)
 	})
 
 	t.Run("saying 'Hello, World!', if empty string is passed", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, world!"
+		got := Hello("", "English")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("if language not recognized, English is set by default", func(t *testing.T) {
+		got := Hello("Maxim", "Umba-yumba")
+		want := "Hello, Maxim"
 		assertCorrectMessage(t, got, want)
 	})
 }
