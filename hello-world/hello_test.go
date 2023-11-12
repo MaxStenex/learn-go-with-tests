381f6ffe0f458559965b643653f50b398695e60c
Maxim <yourstenex@gmail.com>
2023-11-12 17:47:25 +0300

    refactored hello world tests, added params in Hello func

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,8 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Maxim")
-	want := "Hello, Maxim"
+	t.Run("saying hello to the people", func(t *testing.T) {
+		got := Hello("Maxim")
+		want := "Hello, Maxim"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying 'Hello, World!', if empty string is passed", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
