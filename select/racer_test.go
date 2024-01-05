9d0f6a40e01212da88037e23e55e7e15aa598d7e
Maxim <yourstenex@gmail.com>
2024-01-05 18:23:15 +0300

    select

---
+++ select/racer_test.go
@@ -0,0 +1,49 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	t.Run("returns value from fastest server", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("returns an error, if server doesnt respond within 20ms", func(t *testing.T) {
+		serverA := makeDelayedServer(20 * time.Millisecond)
+		serverB := makeDelayedServer(21 * time.Millisecond)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := ConfigurableRacer(serverA.URL, serverB.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but didnt get one")
+		}
+	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
